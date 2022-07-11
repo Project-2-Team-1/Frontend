63df82ca8fb8930687e1db689be2dbab1d05a6ea
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-07-11 16:16:22 -0500

    old commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { UserComponent } from './components/user/user.component';
+import { SearchResultsComponent } from './components/search-results/search-results.component';
+import { ParkComponent } from './components/park/park.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserComponent,
+    SearchResultsComponent,
+    ParkComponent
   ],
   imports: [
     BrowserModule,
