3e936f4d73ca44e72bfe0a7f0df25f3d1962125d
LeonardG1993 <leonardguilloryiv@gmail.com>
2022-07-12 13:11:50 -0500

    user component edits

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,15 +1,18 @@
+import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { throwError, Observable, catchError } from 'rxjs';
+import { url } from 'src/environments/environment';
+import { User } from 'src/app/models/user';
+
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  
 
 }
