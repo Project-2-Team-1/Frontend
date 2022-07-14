a23d173149e14e90f7a72f8bc9e916d0120403ec
cmseibel <cam.m.seib@gmail.com>
2022-07-14 13:19:54 -0700

    Write most functionallity for user dashboard

---
+++ src/app/components/user/placeholder.ts
@@ -0,0 +1,26 @@
+export const USER_PLACEHOLDER = {
+	"id": 1,
+	"firstName": "Cameron",
+	"lastName": "Seibel",
+	"username": "camms",
+	"password": "password",
+	"email": "camms@gmail.com",
+	"reviews": [
+		{
+			"id": 2,
+			"rating": 0,
+			"content": "",
+			"dateReviewed": null,
+			"parkCode": "ebla",
+			"user": 1
+		},
+		{
+			"id": 1,
+			"rating": 5,
+			"content": "Very nice mountain I like the pretty flowers.",
+			"dateReviewed": null,
+			"parkCode": "mora",
+			"user": 1
+		}
+	]
+}
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,8 +1,8 @@
+import { USER_PLACEHOLDER } from './placeholder';
+import { DataService } from './../../service/data.service';
+import { Router } from '@angular/router';
 import { ParkService } from './../../service/park.service';
-import { UserService } from 'src/app/service/user.service';
 import { Component, OnInit } from '@angular/core';
-import { User } from 'src/app/models/user';
-import { AppComponent } from 'src/app/app.component';
 import { AuthService } from 'src/app/service/auth.service';
 
 
@@ -15,15 +15,31 @@ import { AuthService } from 'src/app/service/auth.service';
 export class UserComponent implements OnInit {
   
   user: any;
+  parks: any[] = [];
 
-  constructor(public ParkService: UserService) { }
+  constructor(private authService: AuthService, private dataService: DataService, private parkService: ParkService, private router: Router) { }
 
   userData(): any{      
     
   } 
 
   ngOnInit(): void {
-
+    // this.dataService.user = USER_PLACEHOLDER;
+    if(!this.authService.isLoggedIn()){
+      this.router.navigate(["/login"]);
+    }
+    this.user = this.dataService.user; // For live
+    let codes = [];
+    console.log(this.user.reviews);
+    for(let r of this.user.reviews) {
+      codes.push(r.parkCode);
+    }
+    console.log(codes);
+    this.parkService.getParksByParkCode(codes)
+      .subscribe((result) => {
+        console.log(result);
+        this.parks = result.data;
+      });
   }
 
 }
