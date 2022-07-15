50aef9b8c8dd9f18db6656314ab42fbf22d281ef
Jen Osborne <jander38@yahoo.com>
2022-07-15 06:33:30 -0400

    changing buildspec

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,3 +3,7 @@ export const environment = {
 };
 
 
+export const url = `http://localhost:5000/api`
+
+export const API_KEY = "OcqX9auv98CBweX5IEtQZIYRoYST7u883NZPSkSo";
+export const PARKS_BASE_URL = `https://developer.nps.gov/api/v1`
